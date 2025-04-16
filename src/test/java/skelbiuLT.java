09f9ddb009e3305692c4283eec53301a31a792c0
Anciado5 <ana.dolgova@gmail.com>
2025-04-16 15:31:34 +0300

    uzbaigtas

--- src/test/java/skelbiuLT.java
+++ src/test/java/skelbiuLT.java
@@ -33,34 +33,37 @@ public class skelbiuLT {
 //            }
 //            System.out.println(price);
 //        }
+//        System.out.println("===================================================================================");
 //    }
 
-    @Test
-    public void openWebsite() throws InterruptedException {
+
+//    @Test
+    public void openWebsite1() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();//reikia parasyti, nes kartais irgi luzta testai, kai per didele rezoliucija, sitas padeda sumazinti erroru tikimybe
+        driver.manage().window().maximize();
         driver.get("https://skelbiu.lt");
-        Thread.sleep(500);//reikia parasyti, ns kartais del to luzta testai, nes nespeja uzkrauti
+        Thread.sleep(500);
         WebElement acceptBtn = driver.findElement(By.id("onetrust-accept-btn-handler"));
         acceptBtn.click();
         driver.findElement(By.id("searchKeyword")).sendKeys("verpimo ratelis");
         driver.findElement(By.id("searchButton")).click();
         Thread.sleep(500);
+
         String adsQantity = driver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li/span")).getText();
-        double adsQantityDbl = Double.parseDouble(adsQantity.replaceAll("[^\\d]", ""));
         System.out.println("Radome " + adsQantity + " skelbimu.");
+        double adsQantityDbl = Double.parseDouble(adsQantity.replaceAll("[^\\d]", ""));
         double priceSum = 0;
-        while (true) {//begalinis ciklas, kuris nutraukiamas ivykus kazkuriai tai salygai
-            WebElement container = driver.findElement(By.className("standard-list-container"));
-            List<WebElement> contentBlocks = container.findElements(By.className("content-block"));
+        while (true) {
+            WebElement container1 = driver.findElement(By.className("standard-list-container"));
+            List<WebElement> contentBlocks = container1.findElements(By.className("content-block"));
             Thread.sleep(1000);
-            for (int i = 0; i < contentBlocks.size(); i++) {//foras yra kiekvienam skelbimui puslapyje
+            for (int i = 0; i < contentBlocks.size(); i++) {
                 String price = "0";
                 double priceDbl = 0;
                 try {
                     price = contentBlocks.get(i).findElement(By.className("price")).getText();
                 }
-                catch (Exception e) {}//try catch --> gaudo klaidas ir arba su jom kazka daro, arba nedaro nieko (priklausomai koks scenarijus)
+                catch (Exception e) {}
                 priceDbl = Double.parseDouble(price.replaceAll("[^\\d,]", ""));
                 priceSum += priceDbl;
                 System.out.println(price + " " + priceDbl);
@@ -75,24 +78,61 @@ public class skelbiuLT {
         }
         System.out.println("Total sum = " + priceSum);
         System.out.printf("Average price =  %.2f", priceSum / adsQantityDbl);
+        System.out.println("===================================================================================");
+    }
 
+    @Test
+    public void openWebsite2() throws InterruptedException{
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://skelbiu.lt");
+        Thread.sleep(500);
+        WebElement acceptBtn = driver.findElement(By.id("onetrust-accept-btn-handler"));
+        acceptBtn.click();
+        driver.findElement(By.id("searchKeyword")).sendKeys("banglente");
+        driver.findElement(By.id("searchButton")).click();
+        Thread.sleep(500);
 
+        String adsQantity = driver.findElement(By.xpath("//*[@id=\"body-container\"]/div[2]/div[1]/ul/li/span")).getText();
+        System.out.println("Radome " + adsQantity + " skelbimu.");
 
+        while (true) {
+            WebElement container2 = driver.findElement(By.className("standard-list-container"));
+            List<WebElement> contentBlocks2 = container2.findElements(By.className("content-block"));
+            Thread.sleep(1000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            for (int i = 0; i < contentBlocks2.size(); i++) {
+                String url = driver.getCurrentUrl();
+                String name = "0";
+                try {
+                    contentBlocks2.get(i).click();
+                    Thread.sleep(1000);
+                    name = contentBlocks2.get(i).findElement(By.xpath("//*[@id=\"user-info-container\"]/div[1]/div[2]/div/div[1]")).getText();
+                    String itemUrl = driver.getCurrentUrl();
+                    Thread.sleep(1000);
+                    System.out.println(itemUrl);
+                }
+                catch (Exception e) {
+//                    System.out.println("Nera vardo");
+                }
+                try {
+                    driver.get(url);
+                    Thread.sleep(1000);
+                    container2 = driver.findElement(By.className("standard-list-container"));
+                    contentBlocks2 = container2.findElements(By.className("content-block"));
+                }
+                catch (Exception e){
+                    break;
+                }
+            }
+            WebElement paginationBlock1 = driver.findElement(By.id("pagination"));
+            try {
+                WebElement lastElement = paginationBlock1.findElement(By.linkText("»"));
+                lastElement.click();
+            } catch (Exception e) {
+                break;
+            }
+        }
 
 
 
